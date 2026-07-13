a2d88ec0cac8fdd8a138762a5701d4648e58260f
mehdigaray-web <mehdigaray@gmail.com>
2026-07-13 15:31:31 +0400

    Refactor device classes and implement constructors

--- Lesson_10_/Program.cs
+++ Lesson_10_/Program.cs
@@ -1,248 +1,428 @@
-﻿//1
-//public abstract class Device
-//{
-//    public string Name;
-//    public string Characteristics;
+// task 1
+class Device
+{
+    public string Name;
+    public string Brand;
 
-//    public Device(string name, string characteristics)
-//    {
-//        Name = name;
-//        Characteristics = characteristics;
-//    }
+    public Device(string name, string brand)
+    {
+        Name = name;
+        Brand = brand;
+    }
 
-//    public void Show()
-//    {
-//        Console.WriteLine($"Device: {Name}");
-//    }
+    public virtual void Sound()
+    {
+        Console.WriteLine("...");
+    }
 
-//    public void Desc()
-//    {
-//        Console.WriteLine($"Characteristics: {Characteristics}");
-//    }
+    public virtual void Show()
+    {
+        Console.WriteLine("Device: " + Name);
+    }
 
-//    public abstract void Sound();
-//}
+    public virtual void Desc()
+    {
+        Console.WriteLine("Brand: " + Brand);
+    }
+}
 
-//public class Kettle : Device
-//{
-//    public Kettle(string name, string characteristics) : base(name, characteristics) { }
+class Kettle : Device
+{
+    public int Volume;
+    public int Power;
 
-//    public override void Sound()
-//    {
-//        Console.WriteLine("Sound: Whoosh (water boiling) and whistle!");
-//    }
-//}
+    public Kettle(string name, string brand, int volume, int power) : base(name, brand)
+    {
+        Volume = volume;
+        Power = power;
+    }
 
-//public class Microwave : Device
-//{
-//    public Microwave(string name, string characteristics) : base(name, characteristics) { }
+    public override void Sound()
+    {
+        Console.WriteLine("Shhhhh... click!");
+    }
 
-//    public override void Sound()
-//    {
-//        Console.WriteLine("Sound: Bzzzz... Ding!");
-//    }
-//}
+    public override void Show()
+    {
+        Console.WriteLine("Kettle: " + Name);
+    }
 
-//public class Car : Device
-//{
-//    public Car(string name, string characteristics) : base(name, characteristics) { }
+    public override void Desc()
+    {
+        Console.WriteLine("Brand: " + Brand + ", Volume: " + Volume + "L, Power: " + Power + "W");
+    }
+}
 
-//    public override void Sound()
-//    {
-//        Console.WriteLine("Sound: Vroom-vroom! Beep-beep!");
-//    }
-//}
+class Microwave : Device
+{
+    public int Power;
+    public double CameraVolume;
 
-//public class Steamboat : Device
-//{
-//    public Steamboat(string name, string characteristics) : base(name, characteristics) { }
+    public Microwave(string name, string brand, int power, double cameraVolume) : base(name, brand)
+    {
+        Power = power;
+        CameraVolume = cameraVolume;
+    }
 
-//    public override void Sound()
-//    {
-//        Console.WriteLine("Sound: Tuuuu-tuuuu!");
-//    }
-//}
+    public override void Sound()
+    {
+        Console.WriteLine("Ding!");
+    }
 
-//class Program
-//{
-//    static void Main()
-//    {
-//        Console.WriteLine("--- TASK 1: DEVICES ---");
+    public override void Show()
+    {
+        Console.WriteLine("Microwave: " + Name);
+    }
 
-//        Device myKettle = new Kettle("Tefal", "Electric, 2 Liters, 2000W");
-//        myKettle.Show();
-//        myKettle.Desc();
-//        myKettle.Sound();
+    public override void Desc()
+    {
+        Console.WriteLine("Brand: " + Brand + ", Power: " + Power + "W, Volume: " + CameraVolume + "L");
+    }
+}
 
-//        Console.WriteLine();
+class Car : Device
+{
+    public int Speed;
+    public string FuelType;
 
-//        Device myCar = new Car("Toyota Camry", "Sedan, 2.5L Engine, Automatic");
-//        myCar.Show();
-//        myCar.Desc();
-//        myCar.Sound();
+    public Car(string name, string brand, int speed, string fuelType) : base(name, brand)
+    {
+        Speed = speed;
+        FuelType = fuelType;
+    }
 
-//        Console.WriteLine("\nPress Enter to exit...");
-//        Console.ReadLine();
-//    }
-//}
+    public override void Sound()
+    {
+        Console.WriteLine("Vroom vroom!");
+    }
 
+    public override void Show()
+    {
+        Console.WriteLine("Car: " + Name);
+    }
 
-//2
+    public override void Desc()
+    {
+        Console.WriteLine("Brand: " + Brand + ", Max speed: " + Speed + "km/h, Fuel: " + FuelType);
+    }
+}
 
-//public abstract class MusicalInstrument
-//{
-//    public string Name;
-//    public string Characteristics;
-//    public string HistoryText;
+class Steamship : Device
+{
+    public int Capacity;
+    public double Length;
 
-//    public MusicalInstrument(string name, string characteristics, string historyText)
-//    {
-//        Name = name;
-//        Characteristics = characteristics;
-//        HistoryText = historyText;
-//    }
+    public Steamship(string name, string brand, int capacity, double length) : base(name, brand)
+    {
+        Capacity = capacity;
+        Length = length;
+    }
 
-//    public void Show()
-//    {
-//        Console.WriteLine($"Instrument: {Name}");
-//    }
+    public override void Sound()
+    {
+        Console.WriteLine("Tuuuuut!!!");
+    }
 
-//    public void Desc()
-//    {
-//        Console.WriteLine($"Description: {Characteristics}");
-//    }
+    public override void Show()
+    {
+        Console.WriteLine("Steamship: " + Name);
+    }
 
-//    public void History()
-//    {
-//        Console.WriteLine($"History: {HistoryText}");
-//    }
+    public override void Desc()
+    {
+        Console.WriteLine("Brand: " + Brand + ", Capacity: " + Capacity + " passengers, Length: " + Length + "m");
+    }
+}
 
-//    public abstract void Sound();
-//}
+// Main
 
-//public class Violin : MusicalInstrument
-//{
-//    public Violin(string name, string characteristics, string historyText) : base(name, characteristics, historyText) { }
+Kettle kettle = new Kettle("AquaBoil 3000", "Philips", 2, 2200);
+Microwave microwave = new Microwave("QuickHeat X5", "Samsung", 900, 25);
+Car car = new Car("Model S", "Tesla", 250, "Electric");
+Steamship steamship = new Steamship("Atlantic Queen", "Carnival", 3000, 320);
 
-//    public override void Sound()
-//    {
-//        Console.WriteLine("Sound: Smooth and high-pitched string vibration.");
-//    }
-//}
+List<Device> devices = new List<Device>();
+devices.Add(kettle);
+devices.Add(microwave);
+devices.Add(car);
+devices.Add(steamship);
 
-//public class Trombone : MusicalInstrument
-//{
-//    public Trombone(string name, string characteristics, string historyText) : base(name, characteristics, historyText) { }
+for (int i = 0; i < devices.Count; i++)
+{
+    devices[i].Show();
+    devices[i].Desc();
+    devices[i].Sound();
+    Console.WriteLine();
+}
 
-//    public override void Sound()
-//    {
-//        Console.WriteLine("Sound: Loud and low brass honk.");
-//    }
-//}
+// task 2
 
-//public class Ukulele : MusicalInstrument
-//{
-//    public Ukulele(string name, string characteristics, string historyText) : base(name, characteristics, historyText) { }
+class MusicalInstrument
+{
+    public string Name;
+    public string Origin;
+    public int StringCount;
 
-//    public override void Sound()
-//    {
-//        Console.WriteLine("Sound: Light strumming of a small string instrument.");
-//    }
-//}
+    public MusicalInstrument(string name, string origin, int stringCount)
+    {
+        Name = name;
+        Origin = origin;
+        StringCount = stringCount;
+    }
 
-//public class Cello : MusicalInstrument
-//{
-//    public Cello(string name, string characteristics, string historyText) : base(name, characteristics, historyText) { }
+    public virtual void Sound()
+    {
+        Console.WriteLine("...");
+    }
 
-//    public override void Sound()
-//    {
-//        Console.WriteLine("Sound: Deep, rich, and low string resonance.");
-//    }
-//}
+    public virtual void Show()
+    {
+        Console.WriteLine("Instrument: " + Name);
+    }
 
-//class Program
-//{
-//    static void Main()
-//    {
-//        Console.WriteLine("--- TASK 2: MUSICAL INSTRUMENTS ---");
+    public virtual void Desc()
+    {
+        Console.WriteLine("Origin: " + Origin + ", Strings: " + StringCount);
+    }
 
-//        MusicalInstrument myViolin = new Violin("Stradivarius Model", "Acoustic, 4 strings", "Invented in Italy in the 16th century.");
-//        myViolin.Show();
-//        myViolin.Desc();
-//        myViolin.History();
-//        myViolin.Sound();
+    public virtual void History()
+    {
+        Console.WriteLine("History unknown.");
+    }
+}
 
-//        Console.WriteLine();
+class Violin : MusicalInstrument
+{
+    public Violin() : base("Violin", "Italy", 4) { }
 
-//        MusicalInstrument myTrombone = new Trombone("Yamaha Standard", "Brass, slide action", "Evolved from the sackbut in the 15th century.");
-//        myTrombone.Show();
-//        myTrombone.Desc();
-//        myTrombone.History();
-//        myTrombone.Sound();
+    public override void Sound()
+    {
+        Console.WriteLine("Squeeeak... eeee...");
+    }
 
-//        Console.WriteLine("\nPress Enter to exit...");
-//        Console.ReadLine();
-//    }
-//}
+    public override void Show()
+    {
+        Console.WriteLine("Instrument: Violin");
+    }
 
-//3
+    public override void Desc()
+    {
+        Console.WriteLine("Origin: Italy, Strings: 4, Type: Bowed");
+    }
 
-//public abstract class Worker
-//{
-//    public abstract void Print();
-//}
+    public override void History()
+    {
+        Console.WriteLine("The violin appeared in northern Italy in the early 16th century. " +
+            "It evolved from earlier bowed instruments like the rebec and lira da braccio.");
+    }
+}
 
-//public class President : Worker
-//{
-//    public override void Print()
-//    {
-//        Console.WriteLine("Position: President. Duties: Company management, making key decisions.");
-//    }
-//}
+class Trombone : MusicalInstrument
+{
+    public Trombone() : base("Trombone", "Europe", 0) { }
 
-//public class Security : Worker
-//{
-//    public override void Print()
-//    {
-//        Console.WriteLine("Position: Security Guard. Duties: Ensuring safety of staff and property.");
-//    }
-//}
+    public override void Sound()
+    {
+        Console.WriteLine("Bwaaaaah... wah wah...");
+    }
 
-//public class Manager : Worker
-//{
-//    public override void Print()
-//    {
-//        Console.WriteLine("Position: Manager. Duties: Process organization, working with clients and staff.");
-//    }
-//}
+    public override void Show()
+    {
+        Console.WriteLine("Instrument: Trombone");
+    }
 
-//public class Engineer : Worker
-//{
-//    public override void Print()
-//    {
-//        Console.WriteLine("Position: Engineer. Duties: Development, design, and technical support.");
-//    }
-//}
+    public override void Desc()
+    {
+        Console.WriteLine("Origin: Europe, Type: Brass wind, No strings");
+    }
 
-//class Program
-//{
-//    static void Main()
-//    {
-//        Console.WriteLine("--- TASK 3: WORKERS ---");
+    public override void History()
+    {
+        Console.WriteLine("The trombone was developed in the 15th century in Burgundy. " +
+            "It was originally called the sackbut and was widely used in church music.");
+    }
+}
 
-//        Worker manager = new Manager();
-//        manager.Print();
+class Ukulele : MusicalInstrument
+{
+    public Ukulele() : base("Ukulele", "Portugal/Hawaii", 4) { }
 
-//        Worker engineer = new Engineer();
-//        engineer.Print();
+    public override void Sound()
+    {
+        Console.WriteLine("Dum dum da-da dum...");
+    }
 
-//        Worker president = new President();
-//        president.Print();
+    public override void Show()
+    {
+        Console.WriteLine("Instrument: Ukulele");
+    }
 
-//        Worker security = new Security();
-//        security.Print();
+    public override void Desc()
+    {
+        Console.WriteLine("Origin: Portugal/Hawaii, Strings: 4, Type: Plucked");
+    }
 
-//        Console.WriteLine("\nPress Enter to exit...");
-//        Console.ReadLine();
-//    }
-//}
+    public override void History()
+    {
+        Console.WriteLine("The ukulele was introduced to Hawaii in 1879 by Portuguese immigrants. " +
+            "It became a symbol of Hawaiian culture and gained worldwide popularity in the 20th century.");
+    }
+}
+
+class Cello : MusicalInstrument
+{
+    public Cello() : base("Cello", "Italy", 4) { }
+
+    public override void Sound()
+    {
+        Console.WriteLine("Mmmmm... ooohhh...");
+    }
+
+    public override void Show()
+    {
+        Console.WriteLine("Instrument: Cello");
+    }
+
+    public override void Desc()
+    {
+        Console.WriteLine("Origin: Italy, Strings: 4, Type: Bowed, tuning: C-G-D-A");
+    }
+
+    public override void History()
+    {
+        Console.WriteLine("The cello emerged in Italy during the 16th century. " +
+            "Its deep, rich tone made it a staple of orchestras and chamber music by the 18th century.");
+    }
+}
+
+// Main
+
+List<MusicalInstrument> instruments = new List<MusicalInstrument>();
+instruments.Add(new Violin());
+instruments.Add(new Trombone());
+instruments.Add(new Ukulele());
+instruments.Add(new Cello());
+
+for (int i = 0; i < instruments.Count; i++)
+{
+    instruments[i].Show();
+    instruments[i].Desc();
+    instruments[i].Sound();
+    instruments[i].History();
+    Console.WriteLine();
+}
+
+//task 3
+
+abstract class Worker
+{
+    public string Name;
+    public int Age;
+    public double Salary;
+
+    public Worker(string name, int age, double salary)
+    {
+        Name = name;
+        Age = age;
+        Salary = salary;
+    }
+
+    public abstract void Print();
+}
+
+class President : Worker
+{
+    public string Company;
+
+    public President(string name, int age, double salary, string company) : base(name, age, salary)
+    {
+        Company = company;
+    }
+
+    public override void Print()
+    {
+        Console.WriteLine("Position: President");
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Age: " + Age);
+        Console.WriteLine("Company: " + Company);
+        Console.WriteLine("Salary: $" + Salary);
+    }
+}
+
+class Security : Worker
+{
+    public string Post;
+
+    public Security(string name, int age, double salary, string post) : base(name, age, salary)
+    {
+        Post = post;
+    }
+
+    public override void Print()
+    {
+        Console.WriteLine("Position: Security");
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Age: " + Age);
+        Console.WriteLine("Post: " + Post);
+        Console.WriteLine("Salary: $" + Salary);
+    }
+}
+
+class Manager : Worker
+{
+    public string Department;
+    public int TeamSize;
+
+    public Manager(string name, int age, double salary, string department, int teamSize) : base(name, age, salary)
+    {
+        Department = department;
+        TeamSize = teamSize;
+    }
+
+    public override void Print()
+    {
+        Console.WriteLine("Position: Manager");
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Age: " + Age);
+        Console.WriteLine("Department: " + Department);
+        Console.WriteLine("Team size: " + TeamSize);
+        Console.WriteLine("Salary: $" + Salary);
+    }
+}
+
+class Engineer : Worker
+{
+    public string Specialization;
+    public int Experience;
+
+    public Engineer(string name, int age, double salary, string specialization, int experience) : base(name, age, salary)
+    {
+        Specialization = specialization;
+        Experience = experience;
+    }
+
+    public override void Print()
+    {
+        Console.WriteLine("Position: Engineer");
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Age: " + Age);
+        Console.WriteLine("Specialization: " + Specialization);
+        Console.WriteLine("Experience: " + Experience + " years");
+        Console.WriteLine("Salary: $" + Salary);
+    }
+}
+
+// Main
+
+List<Worker> workers = new List<Worker>();
+workers.Add(new President("Ilham Aliyev", 52, 15000, "SOCAR"));
+workers.Add(new Security("Tural Mammadov", 35, 800, "Main entrance"));
+workers.Add(new Manager("Aysel Hasanova", 40, 3500, "Sales", 12));
+workers.Add(new Engineer("Rauf Guliyev", 29, 2200, "Software", 5));
+
+for (int i = 0; i < workers.Count; i++)
+{
+    workers[i].Print();
+    Console.WriteLine();
+}
+
